ebb35803b24180d2090f9a0618688edbf5a2d1bc
Mathias Carignani <mathcarignani@gmail.com>
2017-03-02 09:58:05 -0300

    Added soundcloud examples

--- asdf/js/mostacho.js
+++ asdf/js/mostacho.js
@@ -3,12 +3,26 @@ $(function(){
     var minHeight = $('.home').height();
     if(windowH > minHeight) {                            
         $('.home').css({'height':($(window).height())+'px'});
-    }                                                                               
+    }                      
+
+    // Update the iframe width
+    $('.vimeo-videos iframe').each(function() {
+        var newHeight = $(this).width() * 0.5434782609 + 2;
+        $(this).css({'height': newHeight});
+    });
+
     $(window).resize(function(){
         var windowH = $(window).height();
         if(windowH > minHeight) {                            
             $('.home').css({'height':($(window).height())+'px'});
         }
+
+        // Update the iframe width
+        $('.vimeo-videos iframe').each(function() {
+            var newHeight = $(this).width() * 0.55;
+            $(this).css({'height': newHeight});
+        });
+
     })          
 });
 
